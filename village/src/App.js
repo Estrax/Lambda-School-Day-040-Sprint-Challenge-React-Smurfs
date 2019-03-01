6e28aeb9f9ce7969c87cbb7e61ff0e6016c22cee
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-01 11:46:18 -0500

    added navbar - finished stage 2

--- village/src/App.js
+++ village/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, NavLink } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -48,8 +48,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="/">Home</Link>
-        <Link to="/smurf-form">New smurf</Link>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/smurf-form">New smurf</NavLink>
+        </nav>
+        
         <Switch>
           <Route path="/" exact render={(props) => <Smurfs smurfs={this.state.smurfs} />}/>
           <Route path="/smurf-form" exact render={(props) => <SmurfForm addSmurf={this.addSmurf} />}/>
